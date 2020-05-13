724a0682b5eb52794d54b8ad99fac8853745ca16
Jiulong Wang <jiulongw@gmail.com>
2020-05-13 15:19:58 -0700

    Add webpack configuration for WASM files

--- packages/react-scripts/config/webpack.config.js
+++ packages/react-scripts/config/webpack.config.js
@@ -535,6 +535,14 @@ module.exports = function(webpackEnv) {
                 'sass-loader'
               ),
             },
+            {
+              test: /\.wasm$/,
+              loader: require.resolve('file-loader'),
+              type: 'javascript/auto',
+              options: {
+                name: 'static/wasm/[name].[hash:8].[ext]',
+              },
+            },
             // "file" loader makes sure those assets get served by WebpackDevServer.
             // When you `import` an asset, you get its (virtual) filename.
             // In production, they would get copied to the `build` folder.
